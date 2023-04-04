eadc1a47b410d65a65a108589b7db011eba3992d
cepedagm14 <cepedagm14@gmail.com>
2023-04-03 23:56:01 -0400

    emoji, time, reactions

--- src/app/features/post/postSlices.js
+++ src/app/features/post/postSlices.js
@@ -1,15 +1,32 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { sub } from "date-fns";
 
 const initialState = [
   {
     id: 1,
     title: "learning redux tool kit",
     content: "aprendiendo redux con dave gray",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: 2,
     title: "aprendiendo hacer slices,reducers, actions",
     content: "no se ve tan dificil ",
+    date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 
@@ -28,12 +45,27 @@ export const postsSlice = createSlice({
             title,
             content,
             userId,
+            date: new Date().toISOString(),
+            reactions: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
       },
+    },
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload;
+      const existingPost = state.find((post) => post.id === postId);
+      if (existingPost) {
+        existingPost.reactions[reaction]++;
+      }
     },
   },
 });
 
 export const selectAllPost = (state) => state.posts;
-export const { postAdded } = postsSlice.actions;
+export const { postAdded, reactionAdded } = postsSlice.actions;
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -2,20 +2,28 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { selectAllPost } from "../app/features/post/postSlices";
 import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionsButtom from "./ReactionButtom";
 
 const PostList = () => {
   const posts = useSelector(selectAllPost);
 
-  const renderedPost = posts.map((post) => (
+  const orderedPost = posts
+    .slice()
+    .sort((a, b) => b.date.localeCompare(a.date));
+
+  const renderedPost = orderedPost.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <p className="postCredit">
         <PostAuthor userId={post.userId} />
+        <TimeAgo timestamp={post.date} />
+        <ReactionsButtom post={post} />
       </p>
     </article>
   ));
-
+console.log(orderedPost);
   return (
     <section>
       <h2>Posts</h2>
